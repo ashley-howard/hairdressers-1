ebe83854c7856442c869ef68387b45b2bdfeae90
Ashley Howard <me@ashley.how>
2020-08-11 11:27:50 +0100

    add menu animation

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+const topMenu = document.getElementById('top-menu');
+
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > vh || document.documentElement.scrollTop > vh) {
+        topMenu.style.position = 'fixed';
+        topMenu.style.backgroundColor = '#1f1f1f';
+        topMenu.style.animation = 'menu 0.4s forwards';
+
+    } else {
+        topMenu.style.position = 'absolute';
+        topMenu.style.backgroundColor = 'transparent';
+        topMenu.style.animation = 'none';
+    }
+}
